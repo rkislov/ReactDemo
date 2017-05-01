b88b7e09428673e27b44b270f0c642b280dd319c
Роман Кислов <roman@kislovs.ru>
2017-05-01 16:10:23 +0500

    client  side rendering

---
+++ ReactDemo/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ReactDemo.Models;
+
+namespace ReactDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly IList<CommentModel> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ReactDemo/Scripts/Tutorial.jsx
@@ -0,0 +1,67 @@
+﻿var CommentBox = React.createClass({ 
+    render: function () {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.props.data} />
+                <CommetForm />
+            </div>
+        );
+    }
+});
+
+var CommentList = React.createClass({
+    render: function () {
+        var commentNodes = this.props.data.map(function (comment) {
+            return (
+                <Comment author={comment.author} key={comment.Id}>
+                    {comment.Text}
+                </Comment>
+                );
+        });
+        return (
+            <div className="commentList">
+                {commentNodes}
+            </div>
+            );
+    }
+});
+
+var Comment = React.createClass({
+    rawMarkup: function () {
+        var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    },
+    render: function () {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()}/>
+            </div>
+            );
+    }
+});
+
+var CommetForm = React.createClass({
+    render: function () {
+        return (
+            <div className="commentForm">
+                Hello, world! I'm a commentForm.
+            </div>
+            );
+    }
+});
+
+var data = [
+    { Id: 1, Author: "Daniel Lo Nigro", Text: "Hello ReactJS.NET World!" },
+    { Id: 2, Author: "Pete Hunt", Text: "This is one comment" },
+    { Id: 3, Author: "Jordan Walke", Text: "This is *another* comment" }
+];
+
+ReactDOM.render(
+    <CommentBox data={data}/>,
+    document.getElementById('content')
+);
